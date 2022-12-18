f2edc1648cd17a5d3e11c8387711892bcd2a92b9
hensuu <hensuu@csie.org>
2022-12-18 09:20:12 +0800

    add compatibility < Python 3.10

--- api.py
+++ api.py
@@ -164,109 +164,108 @@ def register(body):
 
 
 def api(client, method, path, header, body):
-    match method:
-        case "GET":
-            if path in ("/", "/index", "/index.html"):  # open main page
-                f = open("pages/index.html")
-                content = f.read()
-                f.close()
-                content = render_page(content, "index", header)
-                response = [Status(200), Content_type("html"), Content_length(content), "", content, ""]
-                response_text = "\r\n".join(response).encode()
-                client.send_response(response_text)
-            elif path in ("/video1", "/video2"):  # open video/audio page
-                page_name = path.split("/")[1].partition("?")[0]
-                f = open(f"pages/{page_name}.html")
-                content = f.read()
-                f.close()
+    if method == "GET":
+        if path in ("/", "/index", "/index.html"):  # open main page
+            f = open("pages/index.html")
+            content = f.read()
+            f.close()
+            content = render_page(content, "index", header)
+            response = [Status(200), Content_type("html"), Content_length(content), "", content, ""]
+            response_text = "\r\n".join(response).encode()
+            client.send_response(response_text)
+        elif path in ("/video1", "/video2"):  # open video/audio page
+            page_name = path.split("/")[1].partition("?")[0]
+            f = open(f"pages/{page_name}.html")
+            content = f.read()
+            f.close()
 
-                content = render_page(content, page_name, header)
-                response = [Status(200), Content_type("html"), Content_length(content), "", content, ""]
+            content = render_page(content, page_name, header)
+            response = [Status(200), Content_type("html"), Content_length(content), "", content, ""]
+            response_text = "\r\n".join(response).encode()
+            client.send_response(response_text)
+        elif path.startswith("/assets/thumbnail"):
+            f = open(path[1:], "rb")
+            content = f.read()
+            f.close()
+            response = [Status(200), Content_type("png"), Content_length(content), "", ""]
+            response_text = "\r\n".join(response).encode()
+            response_text += content
+            response_text += b"\r\n"
+            client.send_response(response_text)
+        elif path.startswith("/streaming/video"):  # open video streaming
+            video_name = path.split("/")[2].partition("?")[0]
+            print(video_name)
+            if client.cap is None or not client.cap.isOpened():
+                client.cap = cv2.VideoCapture(f"./assets/{video_name}.mp4")
+            if client.cap.isOpened():
+                fps = client.cap.get(cv2.CAP_PROP_FPS)
+                response = [Status(200), Content_type("replace"), "", ""]
                 response_text = "\r\n".join(response).encode()
-                client.send_response(response_text)
-            elif path.startswith("/assets/thumbnail"):
-                f = open(path[1:], "rb")
-                content = f.read()
-                f.close()
-                response = [Status(200), Content_type("png"), Content_length(content), "", ""]
-                response_text = "\r\n".join(response).encode()
-                response_text += content
-                response_text += b"\r\n"
-                client.send_response(response_text)
-            elif path.startswith("/streaming/video"):  # open video streaming
-                video_name = path.split("/")[2].partition("?")[0]
-                print(video_name)
-                if client.cap is None or not client.cap.isOpened():
-                    client.cap = cv2.VideoCapture(f"./assets/{video_name}.mp4")
-                if client.cap.isOpened():
-                    fps = client.cap.get(cv2.CAP_PROP_FPS)
-                    response = [Status(200), Content_type("replace"), "", ""]
-                    response_text = "\r\n".join(response).encode()
-                    client.send_response(response_text + "\r\n".encode() + video_player(client))
-                    while client.signal:
-                        client.send_response(video_player(client))
-                        sleep(1 / fps)
-                else:
-                    response = [Status(403), "", ""]
-                    response_text = "\r\n".join(response).encode()
-                    client.send_response(response_text)
-            elif path.startswith("/login"):
-                f = open("pages/login.html")
-                content = f.read()
-                f.close()
-                response = [Status(200), Content_type("html"), Content_length(content), "", content, ""]
-                response_text = "\r\n".join(response).encode()
-                client.send_response(response_text)
+                client.send_response(response_text + "\r\n".encode() + video_player(client))
+                while client.signal:
+                    client.send_response(video_player(client))
+                    sleep(1 / fps)
             else:
-                response = [Status(404), "", ""]
+                response = [Status(403), "", ""]
                 response_text = "\r\n".join(response).encode()
                 client.send_response(response_text)
+        elif path.startswith("/login"):
+            f = open("pages/login.html")
+            content = f.read()
+            f.close()
+            response = [Status(200), Content_type("html"), Content_length(content), "", content, ""]
+            response_text = "\r\n".join(response).encode()
+            client.send_response(response_text)
+        else:
+            response = [Status(404), "", ""]
+            response_text = "\r\n".join(response).encode()
+            client.send_response(response_text)
 
-        case "POST":
-            if path.startswith("/comment"):  # add comment
-                page_name = path.split("/")[2].partition("?")[0]
-                add_comment(page_name, body)
-                response = [Status(303), Location(page_name), "", ""]  # redirect to video page
-                print(response)
-                response_text = "\r\n".join(response).encode()
-                client.send_response(response_text)
-            elif path.startswith("/login"):
-                ret = login(body)
-                if ret:
-                    response = [Status(303), Location(""), Set_cookie({"token": f"{body['username']}", "path": "/"}),
-                                "", ""]  # redirect to Homepage
-                else:
-                    content = """
+    elif method == "POST":
+        if path.startswith("/comment"):  # add comment
+            page_name = path.split("/")[2].partition("?")[0]
+            add_comment(page_name, body)
+            response = [Status(303), Location(page_name), "", ""]  # redirect to video page
+            print(response)
+            response_text = "\r\n".join(response).encode()
+            client.send_response(response_text)
+        elif path.startswith("/login"):
+            ret = login(body)
+            if ret:
+                response = [Status(303), Location(""), Set_cookie({"token": f"{body['username']}", "path": "/"}),
+                            "", ""]  # redirect to Homepage
+            else:
+                content = """
                     <script>
                         alert("Wrong username or password!");
                         window.location.href = "/login";
                     </script>
                     """
-                    response = [Status(401), Content_length(content), "", content, ""]
+                response = [Status(401), Content_length(content), "", content, ""]
 
-                response_text = "\r\n".join(response).encode()
-                client.send_response(response_text)
-            elif path.startswith("/register"):
-                ret = register(body)
-                if ret:
-                    print("register success")
-                    response = [Status(303), Location(""), Set_cookie({"token": f"{body['username']}", "path": "/"}),
-                                "", ""]
-                else:
-                    content = """
+            response_text = "\r\n".join(response).encode()
+            client.send_response(response_text)
+        elif path.startswith("/register"):
+            ret = register(body)
+            if ret:
+                print("register success")
+                response = [Status(303), Location(""), Set_cookie({"token": f"{body['username']}", "path": "/"}),
+                            "", ""]
+            else:
+                content = """
                     <script>
                         alert("Username already exists!");
                         window.location.href = "/login";
                     </script>
                     """
-                    response = [Status(401), Content_length(content), "", content, ""]
-                response_text = "\r\n".join(response).encode()
-                client.send_response(response_text)
+                response = [Status(401), Content_length(content), "", content, ""]
+            response_text = "\r\n".join(response).encode()
+            client.send_response(response_text)
 
-            elif path.startswith("/logout"):
-                page_name = path.split("/")[2].partition("?")[0]
-                print(page_name)
-                response = [Status(303), Location(f"{page_name}"),
-                            Set_cookie({"token": "", "path": "/", "expires": "Thu, 01 Jan 1970 00:00:00 GMT"}), "", ""]
-                response_text = "\r\n".join(response).encode()
-                client.send_response(response_text)
+        elif path.startswith("/logout"):
+            page_name = path.split("/")[2].partition("?")[0]
+            print(page_name)
+            response = [Status(303), Location(f"{page_name}"),
+                        Set_cookie({"token": "", "path": "/", "expires": "Thu, 01 Jan 1970 00:00:00 GMT"}), "", ""]
+            response_text = "\r\n".join(response).encode()
+            client.send_response(response_text)
